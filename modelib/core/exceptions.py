18eda1b1e00449357451c9f1f9f5378ab33e4b56
Gabriel Guarisa <gabrielguarisa@gmail.com>
2024-09-30 12:52:56 -0300

    Simplify exceptions handler

--- modelib/core/exceptions.py
+++ modelib/core/exceptions.py
@@ -1,9 +1,5 @@
-import traceback
-
-from fastapi import FastAPI, Request, status
-from fastapi.exceptions import RequestValidationError
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from starlette.exceptions import HTTPException as StarletteHTTPException
 
 
 class JsonAPIException(Exception):
@@ -46,73 +42,11 @@ class JsonAPIException(Exception):
         )
 
 
-async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
-    response = JSONResponse(
-        status_code=exc.status_code,
-        content={
-            "errors": [
-                {
-                    "status": exc.status_code,
-                    "title": "HTTP Exception",
-                    "detail": {
-                        "message": exc.detail,
-                        "type": "HTTPException",
-                        "kind": "http-exception",
-                    },
-                }
-            ]
-        },
-    )
-
-    return response
-
-
-async def request_validation_exception_handler(
-    request: Request, exc: RequestValidationError
-):
-    response = JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content={"errors": exc.errors()},
-    )
-
-    return response
-
-
 async def json_api_exception_handler(request: Request, exc: JsonAPIException):
     response = exc.to_json_response()
 
     return response
 
 
-async def generic_exception_handler(request: Request, exc: Exception):
-    try:
-        traceback_info = traceback.format_exception(exc)
-    except Exception:
-        traceback_info = "Unknown"
-
-    response = JSONResponse(
-        status_code=500,
-        content={
-            "errors": [
-                {
-                    "status": 500,
-                    "title": "Internal Server Error",
-                    "detail": {
-                        "message": str(exc),
-                        "type": type(exc).__name__,
-                        "kind": "unknown-generic-exception",
-                        "traceback": traceback_info,
-                    },
-                }
-            ]
-        },
-    )
-
-    return response
-
-
 def init_app(app: FastAPI):
-    app.exception_handler(StarletteHTTPException)(custom_http_exception_handler)
-    app.exception_handler(RequestValidationError)(request_validation_exception_handler)
     app.exception_handler(JsonAPIException)(json_api_exception_handler)
-    app.exception_handler(Exception)(generic_exception_handler)
